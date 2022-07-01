074b8253db62e6783b6d1dc8437ec1d05413beef
Shekhar <shshshe@b0be835690eb.ant.amazon.com>
2022-07-01 23:37:56 +0530

    api added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import logo from "./logo.svg";
 import "@aws-amplify/ui-react/styles.css";
-import {
-  withAuthenticator,
-  Button,
-  Heading,
-  Image,
-  View,
-  Card,
-} from "@aws-amplify/ui-react";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import { API } from 'aws-amplify';
+import { withAuthenticator, Button } from '@aws-amplify/ui-react';
+import { listTodos } from './graphql/queries';
+import { createTodo as createTodoMutation, deleteTodo as deleteTodoMutation } from './graphql/mutations';
+
+const initialFormState = { name: '', description: '' }
 
 function App({ signOut }) {
+  const [todos, setTodos] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  async function fetchTodos() {
+    const apiData = await API.graphql({ query: listTodos });
+    setTodos(apiData.data.listTodos.items);
+  }
+
+  async function createTodo() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createTodoMutation, variables: { input: formData } });
+    setTodos([...todos, formData]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteTodo({ id }) {
+    const newTodosArray = todos.filter(todo => todo.id !== id);
+    setTodos(newTodosArray);
+    await API.graphql({ query: deleteTodoMutation, variables: { input: { id } } });
+  }
+
   return (
-    <View className="App">
-      <Card>
-        <Image src={logo} className="App-logo" alt="logo" />
-        <Heading level={1}>We now have Auth!</Heading>
-      </Card>
+    <div className="App">
+      <h1>My Todos App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value })}
+        placeholder="Todo name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value })}
+        placeholder="Todo description"
+        value={formData.description}
+      />
+      <button onClick={createTodo}>Create Todo</button>
+      <div style={{ marginBottom: 30 }}>
+        {
+          todos.map(todo => (
+            <div key={todo.id || todo.name}>
+              <h2>
+                <td>{todo.name}</td>
+                <td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td>{todo.description}</td>
+                <td>&nbsp;</td><td>&nbsp;</td><td>&nbsp;</td><td><button onClick={() => deleteTodo(todo)}>Delete Todo</button></td>
+              </h2>
+            </div>
+          ))
+        }
+      </div>
       <Button onClick={signOut}>Sign Out</Button>
-    </View>
+    </div >
   );
 }
 
